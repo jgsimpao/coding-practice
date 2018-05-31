76509f46fa4a1f24a87cd06e90804bcae3624b39
jgsimpao <joel.simpao@outlook.com>
2018-05-31 18:34:32 +0800

    Sorting Algorithms

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
 include 'utils.php';
 include 'sort.php';
 
-displayResult(new BubbleSort());
-displayResult(new SelectionSort());
+displayResult(new BubbleSort);
+displayResult(new SelectionSort);
+displayResult(new InsertionSort);
--- sort.php
+++ sort.php
@@ -15,8 +15,8 @@ abstract class Sort
     }
 }
 
- class BubbleSort extends Sort
- {
+class BubbleSort extends Sort
+{
     public function __construct() {
         parent::__construct('Bubble Sort');
     }
@@ -32,10 +32,10 @@ abstract class Sort
 
         return $array;
     }
- }
+}
 
- class SelectionSort extends Sort
- {
+class SelectionSort extends Sort
+{
     public function __construct() {
         parent::__construct('Selection Sort');
     }
@@ -45,7 +45,7 @@ abstract class Sort
             $min = $array[$i];
             $minIdx = $i;
 
-            for ($j = $i; $j < count($array); $j++) {
+            for ($j = $i + 1; $j < count($array); $j++) {
                 if($array[$j] < $min) {
                     $min = $array[$j];
                     $minIdx = $j;
@@ -57,4 +57,65 @@ abstract class Sort
 
         return $array;
     }
- }
+}
+
+class InsertionSort extends Sort
+{
+    public function __construct() {
+        parent::__construct('Insertion Sort');
+    }
+
+    public function sort($array) {
+        for ($i = 1; $i < count($array); $i++) {
+            for ($j = $i; $j > 0; $j--) {
+                if($array[$j] < $array[$j - 1]) {
+                    swap($array[$j], $array[$j - 1]);
+                }
+            }
+        }
+
+        return $array;
+    }
+}
+
+class MergeSort extends Sort
+{
+    public function __construct() {
+        parent::__construct('Merge Sort');
+    }
+
+    public function sort($array) {
+        if (count($array) == 1) {
+            return $array;
+        }
+
+        $firstHalf = array_slice($array, 0, count($array) / 2);
+        $secondHalf = array_slice($array, count($array) / 2);
+
+        return merge($firstHalf, $secondHalf);
+    }
+
+    public function merge($firstHalf, $secondHalf) {
+        $array = [];
+
+        while () {
+            
+        }
+
+        for ($i = 0; $i < count($firstHalf); $i++) {
+            if ($firstHalf[$i] < $secondHalf[$j]) {
+                $array[] = $firstHalf[$i];
+                break;
+            }
+        }
+
+        for ($i = 0; $i < count($firstHalf); $i++) {
+            if ($firstHalf[$i] < $secondHalf[$j]) {
+                $array[] = $firstHalf[$i];
+                break;
+            }
+        }
+
+        return $array;
+    }
+}
